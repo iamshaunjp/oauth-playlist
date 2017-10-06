9ece54d3617d73593292c559c51ae6b7c51577b2
shaunoftheshed pelling <shaunoftheshed pelling>
2017-10-06 14:22:20 +0100

    lesson 6 code

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(
     new GoogleStrategy({
         // options for google strategy
+        clientID: /* add your client ID here */,
+        clientSecret: /* add your client secret here */
     }, () => {
         // passport callback function
     });
