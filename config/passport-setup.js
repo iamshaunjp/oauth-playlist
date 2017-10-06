ee0258654ba53b8615cfe4fdaae29f9efaca8013
shaunoftheshed pelling <shaunoftheshed pelling>
2017-10-06 16:21:27 +0100

    lesson 13 code

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -10,14 +10,22 @@ passport.use(
         clientSecret: keys.google.clientSecret,
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
-        console.log('passport callback function fired:');
-        console.log(profile);
-        new User({
-            googleId: profile.id,
-            username: profile.displayName
-        }).save().then((newUser) => {
-            console.log('new user created: ', newUser);
+        // check if user already exists in our own db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+                // do something
+            } else {
+                // if not, create user in our db
+                new User({
+                    googleId: profile.id,
+                    username: profile.displayName
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    // do something
+                });
+            }
         });
     })
 );
