dec60b7c844b7e0d2e44b00059bd3efb81d1fe28
shaunoftheshed pelling <shaunoftheshed pelling>
2017-10-06 16:58:37 +0100

    lesson 21 code

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -30,7 +30,8 @@ passport.use(
                 // if not, create user in our db
                 new User({
                     googleId: profile.id,
-                    username: profile.displayName
+                    username: profile.displayName,
+                    thumbnail: profile._json.image.url
                 }).save().then((newUser) => {
                     console.log('created new user: ', newUser);
                     done(null, newUser);
--- models/user-model.js
+++ models/user-model.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: String,
-    googleId: String
+    googleId: String,
+    thumbnail: String
 });
 
 const User = mongoose.model('user', userSchema);
