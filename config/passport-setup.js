841ce4d0905dfc64048bf1a2058288df86048556
shaunoftheshed pelling <shaunoftheshed pelling>
2017-10-06 14:18:56 +0100

    lesson 5 code

---
+++ config/passport-setup.js
@@ -0,0 +1,10 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+    }, () => {
+        // passport callback function
+    });
+);
