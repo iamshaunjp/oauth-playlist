b858f58073b7c9baf7157c27aaeaf098e651ec9a
shaunoftheshed pelling <shaunoftheshed pelling>
2017-10-06 14:26:47 +0100

    lesson 7 code

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: 'enter your client id here',
+            clientSecret: 'enter your client secret here'
+        }
+};
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./keys');
 
 passport.use(
     new GoogleStrategy({
         // options for google strategy
-        clientID: /* add your client ID here */,
-        clientSecret: /* add your client secret here */
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
     }, () => {
         // passport callback function
     });
