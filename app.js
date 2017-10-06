a5214e7d291745e61416dcf0d7b06982055b9182
shaunoftheshed pelling <shaunoftheshed pelling>
2017-10-06 16:07:50 +0100

    refactor

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
 
 // set view engine
 app.set('view engine', 'ejs');
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log('connected to mongodb');
+});
 
 // set up routes
 app.use('/auth', authRoutes);
