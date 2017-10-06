e8ef82f8e997fb1aabd6cea166d0ab752e515d1f
shaunoftheshed pelling <shaunoftheshed pelling>
2017-10-06 16:36:39 +0100

    lesson 16 code 2

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -20,7 +20,7 @@ router.get('/google', passport.authenticate('google', {
 // callback route for google to redirect to
 // hand control to passport to use code to grab profile info
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    res.send('you reached the redirect URI');
+    res.send(req.user);
 });
 
 module.exports = router;
